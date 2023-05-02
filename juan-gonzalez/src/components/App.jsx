bd53be3aaf60acaf5c98760817b217f63b8ceaf0
jkapa7 <juandaniel5005@gmail.com>
2023-05-01 22:17:18 -0500

    Cambio en la estructura y organizacion de archivos

---
+++ juan-gonzalez/src/components/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Comming Soon!</h1>
+    </div>
+  );
+}
+
+export default App;
