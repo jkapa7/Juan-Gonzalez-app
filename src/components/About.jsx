37b0e825dedcc4a64e4452726d0e905e30435504
jkapa7 <juandaniel5005@gmail.com>
2023-05-17 22:41:20 -0500

    Elimine imagenes que no se utilizaban

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,18 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius?
+        Sunt officia minus commodi hic deleniti, laboriosam vel reprehenderit
+        fuga nesciunt, numquam harum non dolorum! Quis mollitia natus voluptas.
+        Mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eaque, eius? Sunt officia minus commodi hic deleniti, laboriosam vel
+        reprehenderit fuga nesciunt, numquam harum non dolorum! Quis mollitia
+        natus voluptas. Mollitia!
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,11 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>;
+      <h1>Contact</h1>
+      <p>Email:</p>
+      <p>Linkedin:</p>
+      <p>Github:</p>
+      <p>Designed & Built by Juan Daniel Gonzalez</p>
     </div>
   );
 };
