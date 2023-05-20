18a7a6cf1f656f1b5f8e6a2b047723505673ceec
jkapa7 <juandaniel5005@gmail.com>
2023-05-19 23:01:06 -0500

    Agregue al about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,20 @@ const About = () => {
         Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor sit amet
         consectetur adipisicing elit. Eaque, eius? Sunt officia minus commodi
         hic deleniti, laboriosam vel reprehenderit fuga nesciunt, numquam harum
-        non dolorum! Quis mollitia natus voluptas. Mollitia!
+        non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Eaque, eius? Sunt officia minus
+        commodi hic deleniti, laboriosam vel reprehenderit fuga nesciunt,
+        numquam harum non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius? Sunt
+        officia minus commodi hic deleniti, laboriosam vel reprehenderit fuga
+        nesciunt, numquam harum non dolorum! Quis mollitia natus voluptas.
+        Mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eaque, eius? Sunt officia minus commodi hic deleniti, laboriosam vel
+        reprehenderit fuga nesciunt, numquam harum non dolorum! Quis mollitia
+        natus voluptas. Mollitia! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Eaque, eius? Sunt officia minus commodi hic deleniti,
+        laboriosam vel reprehenderit fuga nesciunt, numquam harum non dolorum!
+        Quis mollitia natus voluptas. Mollitia!
       </p>
     </div>
   );
