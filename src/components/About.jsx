e58d6acb98b3487ddcf56d9169e3ed816fb2be43
jkapa7 <juandaniel5005@gmail.com>
2023-05-22 23:02:18 -0500

    Change in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,21 +12,7 @@ const About = () => {
         natus voluptas. Mollitia! Lorem ipsum dolor sit amet consectetur
         adipisicing elit. Eaque, eius? Sunt officia minus commodi hic deleniti,
         laboriosam vel reprehenderit fuga nesciunt, numquam harum non dolorum!
-        Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Eaque, eius? Sunt officia minus commodi
-        hic deleniti, laboriosam vel reprehenderit fuga nesciunt, numquam harum
-        non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Eaque, eius? Sunt officia minus
-        commodi hic deleniti, laboriosam vel reprehenderit fuga nesciunt,
-        numquam harum non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius? Sunt
-        officia minus commodi hic deleniti, laboriosam vel reprehenderit fuga
-        nesciunt, numquam harum non dolorum! Quis mollitia natus voluptas.
-        Mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Eaque, eius? Sunt officia minus commodi hic deleniti, laboriosam vel
-        reprehenderit fuga nesciunt, numquam harum non dolorum! Quis mollitia
-        natus voluptas. Mollitia! Lorem ipsum dolor sit amet consectetur,
-        Mollitia! Lorem ipsum dolor sit amet consectetur
+        Quis mollitia
       </p>
     </div>
   );
