9867a1e1123de71231081d6f9ec94fcb75893871
jkapa7 <juandaniel5005@gmail.com>
2023-05-27 22:42:07 -0500

    Changes in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,16 @@ const About = () => {
         non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
         sit amet consectetur adipisicing elit.
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius?
+        Sunt officia minus commodi hic deleniti, laboriosam vel reprehenderit
+        fuga nesciunt, numquam harum non dolorum! Quis mollitia natus voluptas.
+        Mollitia! Lorem ipsum dolor sit amet conlit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eaque, eius? Sunt officia minus commodi
+        hic deleniti, laboriosam vel reprehenderit fuga nesciunt, numquam harum
+        non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
+        sit amet consectetur adipisicing elit.
+      </p>
     </div>
   );
 };
