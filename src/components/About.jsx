be42882460bbda7ca73a19a0ecd95cc8841ec9f2
jkapa7 <juandaniel5005@gmail.com>
2023-05-30 22:31:26 -0500

    Changes in About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,7 @@ const About = () => {
   return (
     <div className="About">
       <h1>Who I am?</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius?
-        Sunt officia minus commodi hic deleniti, laboriosam vel reprehenderit
-        fuga nesciunt, numquam harum non dolorum! Quis mollitia natus voluptas.
-        Mollitia! Lorem ipsum dolor sit amet conlit. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Eaque, eius? Sunt officia minus commodi
-        hic deleniti, laboriosam vel reprehenderit fuga nesciunt, numquam harum
-        non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
-        sit amet consectetur adipisicing elit.
-      </p>
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius?
         Sunt officia minus commodi hic deleniti, laboriosam vel reprehenderit
