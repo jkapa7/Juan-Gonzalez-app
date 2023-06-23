f6323f7ede7b87108a8541f5f8fbf0bed7f6647c
jkapa7 <juandaniel5005@gmail.com>
2023-06-22 21:23:08 -0500

    Changes in about+

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,16 @@ const About = () => {
         non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
         sit amet consectetur adipisicing elit.
       </p>
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, eius?
+        Sunt officia minus commodi hic deleniti, laboriosam vel reprehenderit
+        fuga nesciunt, numquam harum non dolorum! Quis mollitia natus voluptas.
+        Mollitia! Lorem ipsum dolor sit amet conlit. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eaque, eius? Sunt officia minus commodi
+        hic deleniti, laboriosam vel reprehenderit fuga nesciunt, numquam harum
+        non dolorum! Quis mollitia natus voluptas. Mollitia! Lorem ipsum dolor
+        sit amet consectetur adipisicing elit.
+      </p>
       <button>See my resume!</button>
     </div>
   );
