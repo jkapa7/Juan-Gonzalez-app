f824aa32257f739f82a33e25b812028e0693d485
jkapa7 <juandaniel5005@gmail.com>
2023-07-21 22:19:51 -0500

    part of multiple lenguges added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { projects } from "../data";
+import { projects } from "../data/data";
 
 const Portfolio = () => {
   return (
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,11 +1,23 @@
 import About from "../components/About";
 import Portfolio from "../components/Portfolio";
 import Layout from "../components/Layout";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../components/Button";
+import Translation from "../data/lenguges.json";
 
 function Home() {
   const [darkMode, setDarkMode] = useState(false);
+
+  const [lenguage, setLenguage] = useState("english");
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    if (lenguage == "english") {
+      setContent(Translation.english);
+    } else if (lenguage == "spanish") {
+      setContent(Translation.spanish);
+    }
+  }, [lenguage]);
 
   const handleClick = () => {
     document.documentElement.classList.toggle("dark");
@@ -16,6 +28,20 @@ function Home() {
 
   return (
     <Layout>
+      <select
+        value={lenguage}
+        onChange={(e) => {
+          setLenguage(e.target.value);
+        }}
+      >
+        <option>english</option>
+        <option>spanish</option>
+      </select>
+
+      <div>
+        <h2>{content.title}</h2>
+      </div>
+
       <>
         <Button onClick={handleClick} text={text}></Button>
       </>
